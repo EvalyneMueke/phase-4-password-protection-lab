30b7b967b50d0263b674caf2e7f6cc4eebb55db8
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-22 21:12:54 +0300

    completed lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  # protect_from_forgery with: :null_session
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user
+    unless current_user
+      render json: { errors: "You must be logged in to access this resource" }, status: :unauthorized
+    end
+  end
+
+
 end
